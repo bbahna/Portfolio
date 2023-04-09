5d841e4ebc981e96fbe5e4f84293e7aafa4d836f
bbahna <hyoon.dev@gmail.com>
2023-04-09 13:22:16 +0900

    :card_file_box: Layout 설정(#2)

---
+++ data/metadata.ts
@@ -0,0 +1,7 @@
+const metadata = {
+	title: 'Hyoon',
+	description: '프론트엔드 개발자 윤효정입니다.',
+	author: 'jeong_hYoon',
+};
+
+export default metadata;
---
+++ data/navlinks.ts
@@ -0,0 +1,7 @@
+const navlinks: { title: string; link: string }[] = [
+	{ title: 'Post', link: '/posts' },
+	// { title: 'About', link: '/about' },
+	// { title: 'Project', link: '/project' },
+];
+
+export default navlinks;
