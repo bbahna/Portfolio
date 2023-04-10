098f50bd97b64190f9f0749fdc852881c7ce7076
bbahna <hyoon.dev@gmail.com>
2023-04-10 12:30:31 +0900

    :package: contentlayer 추가(#4)

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
+const { withContentlayer } = require('next-contentlayer');
 
-module.exports = nextConfig
+module.exports = withContentlayer({
+	reactStrictMode: true,
+});
