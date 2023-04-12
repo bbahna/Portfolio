4cb6dfccb1ae214e381f49b30c38d9465f5fff13
bbahna <hyoon.dev@gmail.com>
2023-04-12 12:30:31 +0900

    :page_facing_up: blog UI 작업(#3)

---
+++ pages/posts.tsx
@@ -0,0 +1,34 @@
+import { InferGetStaticPropsType } from 'next';
+import { allPosts } from 'contentlayer/generated';
+import BlogPost from '@/components/BlogPost';
+import Container from '@/components/Container';
+
+const Blog = ({ posts }: InferGetStaticPropsType<typeof getStaticProps>) => {
+	return (
+		<Container>
+			<div className={`mt-10 flex flex-col`}>
+				{posts.map((post) => (
+					<BlogPost
+						date={post.date}
+						title={post.title}
+						des={post.description}
+						slug={post._raw.flattenedPath}
+						key={post._id}
+					/>
+				))}
+			</div>
+		</Container>
+	);
+};
+
+export const getStaticProps = async () => {
+	const posts = allPosts.sort((a, b) => Number(new Date(b.date)) - Number(new Date(a.date)));
+
+	return {
+		props: {
+			posts,
+		},
+	};
+};
+
+export default Blog;
