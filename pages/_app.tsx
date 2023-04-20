5da7d3d1a52140f133cbba388d25498fa735cf23
bbahna <hyoon.dev@gmail.com>
2023-04-20 15:30:31 +0900

    🎨 font 설정(#1)

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,6 @@
 import type { AppProps } from 'next/app';
 import '@/styles/globals.css';
+import '@/styles/fonts/index.css';
 
 export default function App({ Component, pageProps }: AppProps) {
 	return <Component {...pageProps} />;
