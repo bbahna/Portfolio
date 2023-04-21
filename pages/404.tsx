967331247dfc2e3268261c4786a46ef2c73e90f9
bbahna <hyoon.dev@gmail.com>
2023-04-21 14:49:04 +0900

    📄 404 page UI 작업(#3)

---
+++ pages/404.tsx
@@ -0,0 +1,18 @@
+import Link from 'next/link';
+import Container from '@/components/Container';
+import Title from '@/components/Title';
+
+const Notfound = () => {
+	return (
+		<Container>
+			<Title main="🚫 404" description="어쩌다 여기까지 오셨나요." />
+			<Link href="/" legacyBehavior>
+				<a className="block p-2 text-sm border rounded-md border-slate-300 hover:border-slate-400 hover:bg-slate-50">
+					홈으로 가기
+				</a>
+			</Link>
+		</Container>
+	);
+};
+
+export default Notfound;
