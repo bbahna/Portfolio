0e15b84a14912ff2e154da45454ee267478e3d8e
bbahna <hyoon.dev@gmail.com>
2023-04-21 14:49:14 +0900

    📄 about UI 작업(#3)

--- data/navlinks.ts
+++ data/navlinks.ts
@@ -1,7 +1,7 @@
 const navlinks: { title: string; link: string }[] = [
-	{ title: 'Post', link: '/posts' },
-	// { title: 'About', link: '/about' },
-	// { title: 'Project', link: '/project' },
+	{ title: 'Posts', link: '/posts' },
+	{ title: 'About', link: '/about' },
+	{ title: 'Project', link: '/project' },
 ];
 
 export default navlinks;
