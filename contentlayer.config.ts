6c284d6e800bff473f9db5f1d870358c15e206d1
bbahna <hyoon.dev@gmail.com>
2023-05-08 13:22:16 +0900

    📦 code block 추가(#4)

--- contentlayer.config.ts
+++ contentlayer.config.ts
@@ -1,4 +1,5 @@
 import { defineDocumentType, makeSource } from 'contentlayer/source-files';
+import rehypePrettyCode from 'rehype-pretty-code';
 
 export const Post = defineDocumentType(() => ({
 	name: 'Post',
@@ -15,4 +16,14 @@ export const Post = defineDocumentType(() => ({
 export default makeSource({
 	contentDirPath: 'posts',
 	documentTypes: [Post],
+	mdx: {
+		rehypePlugins: [
+			[
+				rehypePrettyCode,
+				{
+					theme: 'material-theme-palenight',
+				},
+			],
+		],
+	},
 });
