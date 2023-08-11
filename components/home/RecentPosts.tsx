90fa9961c05939b4516efbb70c955174d9bde708
bbahna <hyoon.dev@gmail.com>
2023-08-11 20:13:08 +0900

    [home] RecentPosts 3개로 변경

--- components/home/RecentPosts.tsx
+++ components/home/RecentPosts.tsx
@@ -5,7 +5,7 @@ const RecentPosts = ({ posts }: any) => {
 		<section className="mt-10">
 			<h1 className="pb-4 mb-4 text-3xl italic font-extrabold border-b border-1 theme-border-y">Recent Posts 📝</h1>
 			<div className="flex flex-col">
-				{posts.slice(0, 5).map((post: any) => (
+				{posts.slice(0, 3).map((post: any) => (
 					<PostList post={post} slug={post._raw.flattenedPath} key={post._id} />
 				))}
 			</div>
