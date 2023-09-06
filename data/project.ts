9c80bab32240d8de2c00c7b1e9482fe616d4153e
bbahna <hyoon.dev@gmail.com>
2023-09-06 21:13:08 +0900

    refactor : projectProps 추가, export 추가
    - 화이트/다크 모드별 썸네일 (작업중)

--- data/project.ts
+++ data/project.ts
@@ -1,4 +1,5 @@
-const project: {
+export interface projectProps {
+	[x: string]: any;
 	id: number;
 	title: string;
 	category: string;
@@ -8,7 +9,9 @@ const project: {
 	thumbnail?: string;
 	date: string;
 	hashtags: Array<string>;
-}[] = [
+}
+
+const project: projectProps[] = [
 	{
 		id: 1,
 		title: '얼음땡 side project',
@@ -16,6 +19,7 @@ const project: {
 		status: '완료',
 		link: 'https://hyoon-share.notion.site/Next-side-project-6271f98017cf425f8b42013900338180',
 		description: '6개월 이상 현업 개발자의 팀 사이드 프로젝트 커뮤니티',
+		// thumbnail: `bg-[url('/project/project_1.svg')]`,
 		thumbnail: '/project/project_1.svg',
 		date: '2023.5 ~ 2023.7',
 		hashtags: ['비사이드', '팀프로젝트', '아이스브레이킹'],
@@ -27,6 +31,7 @@ const project: {
 		status: '완료',
 		link: 'https://hyoon-share.notion.site/Next-Portfolio-web-site-ab7b701e0f294a24ab9afded438f9bad',
 		description: 'Next로 제작한 개인 포트폴리오 웹 사이트',
+		// thumbnail: `bg-[url('/project/project_2.png')] dark:bg-[url('/project/project_2_dark.png')]`,
 		thumbnail: '/project/project_2.png',
 		date: '2023.4 ~ 2023.5',
 		hashtags: ['개인', '포트폴리오', '다크모드', '반응형'],
@@ -38,6 +43,7 @@ const project: {
 		status: '완료',
 		link: 'https://hyoon-share.notion.site/React-CRUD-project-65d5fbae0b55458585073351af79e3c8',
 		description: 'json-server로 CRUD가 가능한 react 개인 프로젝트',
+		// thumbnail: `bg-[url('/project/project_3.png')]`,
 		thumbnail: '/project/project_3.png',
 		date: '2022.10(1차), 2023.3(2차)',
 		hashtags: ['개인', 'CRUD', '방명록'],
@@ -49,6 +55,7 @@ const project: {
 		status: '완료',
 		link: 'https://hyoon-share.notion.site/Gatsby-Blog-project-e00db9d6957c423399c3b62d94a8c64e',
 		description: 'gatsby template로 제작한 기술 블로그 개인 프로젝트',
+		// thumbnail: `bg-[url('/project/project_4.png')] dark:bg-[url('/project/project_4_dark.png')]`,
 		thumbnail: '/project/project_4.png',
 		date: '2022.5 ~ 2022.6',
 		hashtags: ['개인', '블로그', '템플릿', '커스터마이징'],
