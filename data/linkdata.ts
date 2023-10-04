5395b0da8ba62551ba535bd5b8653bff7ff97d55
bbahna <hyoon.dev@gmail.com>
2023-10-04 20:13:08 +0900

    content : 메인 링크 내용 추가

---
+++ data/linkdata.ts
@@ -0,0 +1,20 @@
+const linkdata: { title: string; description: string; link: string; image: string }[] = [
+	{ title: 'Github', description: '이것 저것 코드 조각', link: 'https://github.com/bbahna', image: '/github.jpg' },
+	{ title: 'Porfolio', description: '직접 만든 포트폴리오 사이트', link: '/home', image: '/og_image.png' },
+	{ title: 'Velog', description: '벨로그 기술 블로그', link: 'https://velog.io/@bbahna', image: '/velog.jpg' },
+	{
+		title: 'Naver Blog',
+		description: '효동동 일상 블로그',
+		link: 'https://m.blog.naver.com/moondomi_',
+		// link: 'https://blog.naver.com/moondomi_',
+		image: '/naver.png',
+	},
+	{
+		title: 'Instagram',
+		description: '소소한 일상 기록',
+		link: 'https://www.instagram.com/jeong_hyoon',
+		image: '/instar.jpg',
+	},
+];
+
+export default linkdata;
