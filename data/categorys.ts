8e8adf0e2efc133a1ac8f3c46ecff4b77b58da38
hyoon <92833235+bbahna@users.noreply.github.com>
2023-11-19 11:39:59 +0900

    content : categorys - Retrospect 추가

--- data/categorys.ts
+++ data/categorys.ts
@@ -1,6 +1,7 @@
 export const categorys: { title: string; keyword: string }[] = [
 	{ title: 'All', keyword: 'all' },
 	{ title: 'Reviewed', keyword: 'Reviewed' },
+	{ title: 'Retrospect', keyword: 'Retrospect' },
 	{ title: 'JavaScript', keyword: 'JavaScript' },
 	{ title: 'Web', keyword: 'Web' },
 	{ title: 'React', keyword: 'React' },
